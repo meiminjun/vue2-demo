ff4b40c0426e854310c1857e83fccd1eb2382c9e
Apple <apple@AppledeMacBook-Air.local>
2016-10-15 12:47:26 +0800

    demo

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>{{ msg }}</h1>
+    <input v-model="msg" type="text" name="">
+    <firstcomponent></firstcomponent>
+  </div>
+</template>
+
+<script>
+import firstcomponent from './component/firstcomponent.vue'
+
+export default {
+  data () {
+    return {
+      msg: 'Hello Vuedfdf22!'
+    }
+  },
+  components:{ firstcomponent }
+}
+</script>
+
+<style>
+body {
+  font-family: Helvetica, sans-serif;
+}
+</style>
---
+++ src/component/firstcomponent.vue
@@ -0,0 +1,20 @@
+<template>
+	<div id="firstcomponent">
+		<h1>I am a title</h1>
+		<a href="">write {{author}}</a>
+	</div>
+</template>
+
+<script type="text/javascript">
+	export default {
+		data() {
+			return {
+				author:'微信公众号神奇'
+			}
+		}
+	}
+</script>
+
+<style type="text/css">
+
+</style>
